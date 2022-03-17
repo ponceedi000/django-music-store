4000ac148fac4dfcaf61da3c13703188ba1fa34f
Ediberto Ponce <ediberto01@gmail.com>
2022-03-16 21:05:09 -0700

    urls route was missing a '/' at end of path

--- music_store_api_project/urls.py
+++ music_store_api_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/music', include('music.urls')),
+    path('api/music/', include('music.urls')),
 ]
