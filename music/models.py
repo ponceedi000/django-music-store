50c9bad4b9f8359052c07328c3659af398fe98c9
Ediberto Ponce <ediberto01@gmail.com>
2022-03-16 21:03:04 -0700

    configured model

--- music/models.py
+++ music/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class Music(models.Model):
   seller = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
   name = models.CharField(max_length=64)
-  instrument = models.TextField()
+  instrument = models.CharField(max_length=24)
   description = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
